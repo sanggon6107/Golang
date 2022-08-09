3571c6cb9f0465a2b2f8bee30409ef933ba507e8
sanggon6107 <sanggon6107@naver.com>
2022-08-09 21:52:21 +0900

    data structure : doubly linked list, queue, map

--- 22-data-structure/22-data-structure.go
+++ 22-data-structure/22-data-structure.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"container/list"
+	"container/ring"
 	"fmt"
 )
 
@@ -18,7 +19,7 @@ func (que *Queue) Front() interface{} {
 	if que.que.Len() == 0 {
 		return nil
 	}
-	return que.que.Back().Value
+	return que.que.Front().Value
 }
 
 func (que *Queue) Len() int {
@@ -29,12 +30,13 @@ func (que *Queue) Pop() {
 	que.que.Remove(que.que.Back())
 }
 
+// 교재는 메소드가 아닌 일반 함수로 구현. Queue객체와의 결합도가 낮아져서 사용자 입장에서는 불편한 것 아닌가?
 func (que *Queue) Create() {
 	que.que = list.New()
 }
 
-// containers/list 의 list는 doubly linked list이다. 즉, 삽입과 삭제가 빈번한 경우에 적합한 불연속 메모리 구조.
 func main() {
+	// containers/list 의 list는 doubly linked list이다. 즉, 삽입과 삭제가 빈번한 경우에 적합한 불연속 메모리 구조.
 	fmt.Println("")
 	firstList := list.New()
 	e1 := firstList.PushBack(4)   // 리턴값(e1)은 추가한 값을 가리킨다.
@@ -52,6 +54,7 @@ func main() {
 	}
 	fmt.Println()
 
+	// queue는 FIFO
 	var q Queue
 	q.Create()
 	q.Push(1)
@@ -61,4 +64,51 @@ func main() {
 		fmt.Print(q.Front(), " ")
 		q.Pop()
 	}
+	fmt.Println()
+
+	// ring은 환형 linked list
+	r := ring.New(5) // 길이를 가진다.
+	ringLen := r.Len()
+
+	for i := 0; i < ringLen; i++ {
+		r.Value = 'A' + i
+		r = r.Next()
+	}
+
+	for j := 0; j < ringLen; j++ {
+		fmt.Print(r.Value, " ")
+		r = r.Next()
+	}
+	fmt.Println()
+
+	// map도 있다. 해쉬맵. 파이썬의 딕셔너리.
+	// Golang의 기본 타입이다. 즉, 어떠한 것도 import할 필요 없다.
+	var myMap map[string]string = map[string]string{} // 교재처럼 myMap := make(map[string]string) 도 가능
+	myMap["key1"] = "value1"
+	myMap["key2"] = "value2"
+	myMap["key3"] = "vakye3"
+
+	fmt.Println(myMap["key1"])
+	fmt.Println(myMap["key2"])
+	fmt.Println(myMap["key3"])
+
+	for k, v := range myMap {
+		fmt.Println("key : ", k, ", value : ", v) // Golang의 맵은 순서 보장하지 않는다.
+	}
+
+	delete(myMap, "key1")
+	delete(myMap, "key2")
+	delete(myMap, "key3")
+
+	// 없는 키값을 찾으면 0을 반환한다. 그렇다면 이것이 진짜 0인지, 아니면 없는 값을 호출해서 0인지 어떻게 아는가?
+	// -> 복수 반환에 답이 있다.
+	val, ret := myMap["key4"]
+	fmt.Println(val) // 0
+	fmt.Println(ret) // false
+
+	// 해시 함수가 따르는 법칙
+	// 1. 같은 입력에 대해 같은 출력을 낸다.
+	// 2. 다른 입력에 대해 웬만하면 다른 입력을 낸다.
+	// 3. 입력값의 범위는 무한대이고 출력값의 범위는 한정되어 있다.
+	// 예) 삼각함수, 나머지 연산.
 }
