9bacdacaedf851e735020abf5aa39871ab7d6d97
sanggon6107 <sanggon6107@naver.com>
2022-09-18 18:34:29 +0900

    how to receive a request as a json and send a response as a json

---
+++ 38-web-server-04/38-web-server-04.go
@@ -0,0 +1,56 @@
+package main
+
+// json handling. This code is about what I've learnt regarding how a server handles json file as a http request.
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type User struct {
+	Name  string    `json:"name"` // matches names between json <-> Golang. annotation.
+	Age   string    `json:"age"`
+	Email string    `json:"email"`
+	Time  time.Time `json:"time"`
+}
+
+type FooHandler struct{}
+
+func (f *FooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	// NewDecoder receives "io.Reader" objects as a parameter, and r.Body is an implementation of io.Reader since it has a method called "Read".
+	// NewDecoder.Decode function returns a decoded object of json.
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	user.Time = time.Now()
+
+	// json.Marshal encodes the argument as []byte(json), and then returns it.
+	// This means that if I want to use this as string, I would need to cast it.
+	data, _ := json.Marshal(user)
+	w.Header().Add("content-type", "application/json") // To let the request header know "data" is a json format.
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, string(data))
+
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Index")
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", IndexHandler)
+	mux.Handle("/Foo", &FooHandler{})
+
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		fmt.Println("err")
+	}
+}
