02fbc2caa5e4be0232e3a3bab635baef2adf361c
sanggon6107 <sanggon6107@naver.com>
2022-09-28 17:26:00 +0900

    RESTful API : Implementing POST

--- 42-web-server-08/myapp/myapp.go
+++ 42-web-server-08/myapp/myapp.go
@@ -1,11 +1,20 @@
 package myapp
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	CreatedAt time.Time `json:"created_at"`
+}
 
 func usersHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Get UserInfo by /users/{id}")
@@ -20,11 +29,27 @@ func getUsersHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "User Id:", vars["id"])
 }
 
-func NewHandler() http.Handler {
-	mux := http.NewServeMux()
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, err)
+		return
+	}
 
+	user.ID = 2
+	user.CreatedAt = time.Now()
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user) // to json format
+	fmt.Fprint(w, string(data))
+}
+
+func NewHandler() http.Handler {
+	mux := mux.NewRouter()
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", createUserHandler).Methods("POST")
 	mux.HandleFunc("/users/{id:[0-9]+}", getUsersHandler)
 
 	return mux
--- 42-web-server-08/myapp/myapp_test.go
+++ 42-web-server-08/myapp/myapp_test.go
@@ -1,12 +1,14 @@
 package myapp
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
+	"strings"
 	"testing"
 )
 
@@ -34,6 +36,7 @@ func TestUsers(t *testing.T) {
 	assert.Contains(string(data), "Get UserInfo")
 }
 
+// test for "Get"
 func TestgetUsersHandler(t *testing.T) {
 	assert := assert.New(t)
 	ts := httptest.NewServer(NewHandler())
@@ -44,5 +47,34 @@ func TestgetUsersHandler(t *testing.T) {
 	assert.NoError(err)
 	assert.Equal(http.StatusOK, resp.StatusCode)
 	data, _ := ioutil.ReadAll(resp.Body) // response
-	assert.Contains(string(data), "User Id:66")
+	assert.Contains(string(data), "User Id:"+userId)
+}
+
+// test for "POST"
+func TestCreateUserHandler(t *testing.T) {
+	assert := assert.New(t)
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Post(ts.URL+"/users", "application/json",
+		strings.NewReader(`{"fist_name":"salcan", "last_name":"Han"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	id := user.ID
+	// to make sure that the id of user created by "POST" equals the id from "GET"
+	resp, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	user2 := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user2) // read the data inside the body of the response.
+	assert.NoError(err)
+	assert.Equal(user.ID, user2.ID)
+	assert.Equal(user.FirstName, user2.FirstName)
 }
